f54a9e0d9a8e0e96bbec4bc61bd534815cabcffa
Alex Royce <alexroyce315@users.noreply.github.com>
2019-12-12 10:01:24 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google、Bing、Baidu 搜索结果屏蔽垃圾 SEO 站点
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       you
 // @match        www.google.com/search?*
@@ -15,7 +15,9 @@
 
     var black_lists = [
         's='
+        ,'q='
         ,'so.php'
+        ,'.xp.cn'
         ,'csdn.net'
         ,'voidcn.com'
         ,'voidcc.com'
@@ -27,23 +29,66 @@
         ,'doulike.me'
         ,'jingcai360.net'
         ,'tuicool.com'
+        ,'398k.com'
+        ,'thinbug.com'
+        ,'bunian.cn'
+        ,'guanfu.net.cn'
+        ,'0818tuan.com'
+        ,'www-599139.com'
+        ,'liqucn.com'
+        ,'18dq.com.cn'
+        ,'wntzx.cn'
+        ,'shoukuanzhifu.com'
+        ,'qz40.com'
+        ,'51ttxue.com'
+        ,'haokan123.net.cn'
+        ,'flyfun.site'
+        ,'9ihb.com'
+        ,'joyk.com'
+        ,'kumaoke.com'
+        ,'3kkg.com'
+        ,'yqhd8.com'
+        ,'ae36.com'
+        ,'wemp.app'
+        ,'smjjl.com'
+        ,'52huixz.com'
+        ,'kumaoke.com'
+        ,'searchman.com'
+        ,'xbw0.com'
+        ,'r8rxt.com'
+        ,'976419.top'
+        ,'wdngh.com'
+        ,'0714news.com'
+        ,'lanmaoyouhui.com'
+        ,'xb636.com'
+        ,'xzjlwscl.com'
+        ,'mgjiaotong.com'
+        ,'029hsmy.com'
+        ,'sxn8452.com'
+        ,'kauuu.cc'
+        ,'xuezishi.net'
+        ,'usd-cny.com'
+        ,'app-store.net.cn'
+        ,'islongs.com'
+        ,'jiechunqiu.com'
+        ,'foufu.com'
     ];
 
     var url = document.location.host;
     console.log('current url', url)
     switch(url) {
         case 'www.google.com':
-            pro_google(black_lists);
+            pro_google();
             break;
         case 'cn.bing.com':
-            pro_bing(black_lists);
+            pro_bing();
             break;
         case 'www.baidu.com':
-            pro_baidu(black_lists);
+            pro_baidu();
             break;
     }
 
-    function pro_google(black_lists) {
+    function pro_google() {
         [...document.getElementsByClassName('r')].map(item => {
             if(item.hasChildNodes()) {
                 var children = item.childNodes;
@@ -59,7 +104,7 @@
         });
     }
 
-    function pro_bing(black_lists) {
+    function pro_bing() {
         [...document.getElementsByClassName('b_attribution')].map(item => {
             if(item.hasChildNodes()) {
                 var children = item.childNodes;
@@ -75,7 +120,7 @@
         });
     }
 
-    function pro_baidu(black_lists) {
+    function pro_baidu() {
         [...document.getElementsByClassName('f13')].map(item => {
             if(item.hasChildNodes()) {
                 var children = item.childNodes;
